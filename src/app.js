ffab7d2ca636284c110c3a56699c46db320163e5
Cristian Muñoz <ing.movil.tic@ipshorisoes.org>
2019-03-26 11:35:24 -0500

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const fs = require('fs');
 const port = 8080;
 require('./helpers');
 
+let cursos = [];
+
 let partialsDirectory = path.join(__dirname, '../partials');
 
 app.set('view engine', 'hbs');
@@ -14,48 +16,129 @@ app.use(bodyParser.urlencoded({ extended: false }));
 hbs.registerPartials(partialsDirectory);
 
 app.get('/', (req, res) => {
-    res.render('course-list');
+    let msg = req.query.success ? 'Matriculado correctamente' : '';
+    res.render('course-list', {
+        coord: 'false',
+        mensaje: msg
+    });
 });
 
 app.get('/crear', (req, res) => {
-    res.render('create-course');
+    let error = req.query.error;
+    if (error) {
+        res.render('create-course', {
+            mensaje: 'Ya existe un curso con ese id.'
+        });
+    } else {
+        res.render('create-course', {
+            mensaje: ''
+        });
+    }
+
 });
 
 app.post('/crear', (req, res) => {
     let curso = req.body;
-    let cursos = [];
     try {
         cursos = require('./cursos.json');
         let exist = cursos.find(cur => cur.id == curso.id);
-        if (exist == undefined) { 
+        console.log("valor de exist " + JSON.stringify(exist));
+        if (!exist) {
+            curso.matriculados = [];
             cursos.push(curso);
-            let list = JSON.stringify(cursos);
-            console.log(list);
-            fs.writeFile('./src/cursos.json', list, (err) => {
-                if (err) console.log('Error guardando curso');
-                console.log('Documento actualizado');
-            })
-            res.redirect('/');
+            guardar();
+            res.redirect('/coordinador?success=true');
         } else {
-            res.send('El curso con id ' + curso.id + ' ya existe');
+            res.redirect('/crear?error=true');
         }
     } catch (err) {
         cursos.push(curso);
-        let list = JSON.stringify(cursos);
-        console.log(list);
-        fs.writeFile('./src/cursos.json', list, (err) => {
-            if (err) console.log('Error guardando curso');
-            console.log('Documento Creado');
-        })
+        guardar();
         res.redirect('/');
     }
 });
 
-app.get('/detalle',(req,res)=>{
+app.get('/detalle', (req, res) => {
     let id = req.query.id;
-    res.send(id);
+    let coord = req.query.coord;
+    let msg = req.query.eliminado?'Estudiante eliminado correctamente':'';
+    cursos = require('./cursos.json');
+    let curso = cursos.find(cur => cur.id == id);
+    res.render('course-detail', {
+        nombre: curso.nombre,
+        descripcion: curso.descripcion,
+        valor: curso.valor,
+        intensidad: curso.intensidad,
+        modalidad: curso.modalidad,
+        id: id,
+        coord: coord,
+        mensaje:msg
+    });
 });
 
+app.get('/registro', (req, res) => {
+    let id = req.query.id;
+    res.render('registro', {
+        id: id
+    });
+});
+
+app.post('/registro', (req, res) => {
+    let estudiante = req.body;
+    cursos = require('./cursos.json');
+    let curso = cursos.find(cur => cur.id == estudiante.id);
+    console.log(curso);
+    let est = curso.matriculados.find(est => est.identificacion == estudiante.identificacion);
+    console.log(curso.matriculados);
+    if (!est) {
+        curso.matriculados.push(estudiante);
+        cursos[curso] = curso;
+        guardar();
+        res.redirect('/?success=true');
+    } else {
+        res.render('registro', {
+            id: estudiante.id,
+            mensaje: 'El estudiante ya esta matriculado en este curso'
+        });
+    }
+});
+
+app.get('/coordinador', (req, res) => {
+    let msg = req.query.success ? 'Curso creado correctamente' : req.query.close ? 'Curso cerrado correctamente' : ''
+
+    res.render('course-list-coordinador', {
+        coord: 'true',
+        mensaje: msg
+    });
+});
+
+app.get('/cerrar-curso', (req, res) => {
+    let id = req.query.id;
+    cursos = require('./cursos.json');
+    curso = cursos.find(cur => cur.id == id);
+    curso.estado = 'cerrado',
+        cursos[curso] = curso;
+    guardar();
+    res.redirect('/coordinador?close=true');
+});
+
+app.post("/eliminar-estudiante", (req, res) => {
+    let body = req.body;
+    cursos = require('./cursos.json');
+    curso = cursos.find(cur => cur.id == body.id);
+    curso.matriculados = curso.matriculados.filter(estu => estu.identificacion != body.identificacion);
+    cursos[curso] = curso;
+    guardar();
+    res.redirect('/detalle?id=' + body.id + '&coord=true&eliminado=true');
+});
+
+let guardar = () => {
+    let list = JSON.stringify(cursos);
+    fs.writeFile('./src/cursos.json', list, (err) => {
+        if (err) console.log('Error guardando curso');
+        console.log('Documento Creado');
+    })
+}
 
 app.listen(port, () => {
     console.log('escuchando en puerto ' + port);
--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,11 @@
 const hbs = require('hbs');
 const fs = require('fs');
 
-hbs.registerHelper('obtenerCursos',()=>{
+hbs.registerHelper('obtenerCursos', (coord) => {
     let cursos = [];
-    try{
+    try {
         cursos = require('./cursos.json');
-    }catch(err){
+    } catch (err) {
         // let text = '[]';
         // fs.writeFile('cursos.json',text,(err)=>{
         //     if(err) console.log('error creando archivo');
@@ -14,10 +14,39 @@ hbs.registerHelper('obtenerCursos',()=>{
         return 'No hay cursos disponibles';
 
     }
-    let text = '<table border=1><thead><th>NOMBRE</th><th>DESCRIPCION</th><th>VALOR</th><th>INTENSIDAD</th><th>MODALIDAD</th><th>DETALLES</th></thead><tbody>';
-    cursos.forEach(curso => {
-        text = text + '<tr><td>'+curso.nombre+'</td><td>'+curso.descripcion+'</td><td>'+curso.valor+'</td><td>'+(curso.intensidad?curso.intensidad:"N/A")+'</td><td>'+(curso.modalidad?curso.modalidad:"N/A")+'</td><td><form action="detalle" method="GET"><input type="hidden" name="id" value="'+curso.id+'"><button>VER</button></form></td><tr>';
+    let text = '<table border=1><thead><th>NOMBRE</th><th>DESCRIPCION</th><th>VALOR</th><th>DETALLES</th></thead><tbody>';
+    let disponibles = cursos.filter(cur => cur.estado == 'disponible');
+    disponibles.forEach(curso => {
+        text = text + '<tr><td>' + curso.nombre + '</td><td>' + curso.descripcion + '</td><td>' + curso.valor + '</td><td><form action="detalle" method="GET"><input type="hidden" name="id" value="' + curso.id + '"><input type="hidden" name="coord" value="' + coord + '"><button>VER</button></form></td><tr>';
     });
     text = text + '</tbody></table>';
-    return text;
+    if (disponibles.length == 0) {
+        return 'No hay cursos disponibles';
+    } else {
+        return text;
+    }
+});
+
+hbs.registerHelper('ifCond', function (coord, options) {
+    if (coord == 'true') {
+        return options.fn(this);
+    }
+    return options.inverse(this);
+});
+
+hbs.registerHelper('obtenerInscritos', (id) => {
+    let cursos = [];
+    cursos = require('./cursos.json');
+    let curso = cursos.find(c=>c.id == id);
+    let text = '<table border=1><thead><th>NOMBRE</th><th>IDENTIFICACION</th><th>CORREO</th><th>TELEFONO</th><th></th></thead><tbody>';
+    curso.matriculados.forEach(est => {
+        text = text + '<tr><td>' + est.nombre + '</td><td>' + est.identificacion + '</td><td>' + est.correo + '</td><td>'+est.telefono+'</td></form></td><td><form action="/eliminar-estudiante" method="POST">'+
+        '<input type="hidden" name="identificacion" value="'+est.identificacion+'"><input type="hidden" name="id" value="'+id+'"><button>Eliminar</button></form></td><tr>';
+    });
+    text = text + '</tbody></table>';
+    if (curso.matriculados.length == 0) {
+        return 'No hay estudiantes matriculados';
+    } else {
+        return text;
+    }
 });
